17d42149e9591265490f21b3164f74852b1ef94c
NightlyDev <66627264+Dyepitech@users.noreply.github.com>
2022-10-17 10:48:35 +0200

    Add files via upload

--- app/Http/Controllers/DevisController.php
+++ app/Http/Controllers/DevisController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Devis;
 use Illuminate\Http\Request;
 
 class DevisController extends Controller
@@ -9,6 +10,27 @@ class DevisController extends Controller
     public function index()
     {
         return view('devis.index', [
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $devis = new Devis;
+
+        $devis->email = $request->input('email');
+        $devis->firstname = $request->input('firstname');
+        $devis->lastname = $request->input('lastname');
+        $devis->phone = $request->input('phone');
+        if ($request->input('situation'))
+            $devis->situation = $request->input('situation');
+        $devis->prestation = $request->input('prestation');
+        $devis->pages = $request->input('pages');
+
+        $devis->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Devis saved',
         ]);
     }
 }

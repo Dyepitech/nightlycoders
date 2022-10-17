533c6b07035fdc4f30a4e54bd5b9ea82ca1e9342
Dyepitech <dylan.trannoy@epitech.eu>
2022-10-17 15:43:03 +0200

    [+] Adding devis migration

---
+++ database/migrations/2022_10_14_095938_devis.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devis', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone');
+            $table->integer('pages');
+            $table->string('prestation');
+            $table->string('situation');
+            $table->string('society')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
